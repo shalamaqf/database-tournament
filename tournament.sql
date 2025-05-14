f5297853cca6dd6b560382c23cad281d67fef87f
Shalama Fadilla <shalamafadillaa@gmail.com>
2025-05-14 14:00:29 +0700

    create a table for tournament

--- tournament.sql
+++ tournament.sql
@@ -4,3 +4,11 @@ create table player
     name varchar(100) not null,
     email varchar(100) not null unique
 );
+
+create table tournament
+(
+    id_tournament serial primary key,
+    tournament_name varchar(100) not null,
+    location varchar(100) not null,
+    tournament_date timestamp default current_timestamp check (tournament_date > current_timestamp)
+);
