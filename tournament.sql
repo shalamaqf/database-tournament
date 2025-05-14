66e1fd0c6e8f6cfaaff75264ee94e23749687bed
Shalama Fadilla <shalamafadillaa@gmail.com>
2025-05-14 14:11:38 +0700

    create a table for registration

--- tournament.sql
+++ tournament.sql
@@ -12,3 +12,12 @@ create table tournament
     location varchar(100) not null,
     tournament_date timestamp default current_timestamp check (tournament_date > current_timestamp)
 );
+
+create table registration
+(
+    id_player int not null,
+    id_tournament int not null,
+    primary key (id_player, id_tournament),
+    constraint fk_player foreign key (id_player) references player(id_player),
+    constraint fk_tournament foreign key (id_tournament) references tournament(id_tournament)
+);
